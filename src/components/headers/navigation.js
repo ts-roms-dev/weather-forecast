75e2624cc5396056d6930ade9be1a407a5bf4f96
ts-roms <107532528+ts-roms@users.noreply.github.com>
2022-07-09 19:52:33 +0800

    - Work on app pages
    - Landing Page
    - Home Page
    - Weather Forecast Page

--- src/components/headers/navigation.js
+++ src/components/headers/navigation.js
@@ -1,27 +1,81 @@
 import React from "react";
-import "../../App.css"
-import logo from '../../logo.svg'
+import {
+  AppBar,
+  Box,
+  Container,
+  Toolbar,
+  Typography,
+  IconButton,
+} from "@mui/material";
+import { Menu as MenuIcon } from "@mui/icons-material";
 
 const Navigation = () => {
-
+  const authenticated = true;
   return (
-    <React.Fragment>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </React.Fragment>
-  )
-}
+    <AppBar position={"static"}>
+      <Container maxWidth={"xl"}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Weather Forecast
+          </Typography>
+          {!authenticated ? (
+            <Box
+              sx={{
+                flexGrow: 0,
+                display: "flex",
+                width: "70%",
+                justifyContent: "flex-end",
+              }}
+            >
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "#fff",
+                  textDecoration: "none",
+                  border: "1px solid rgb(144, 202, 249)",
+                  borderRadius: "4px",
+                  padding: "5px 15px",
+                }}
+              >
+                Logout
+              </Typography>
+            </Box>
+          ) : null}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 
 export default Navigation;
--- src/pages/landing.page.js
+++ src/pages/landing.page.js
@@ -1,13 +1,65 @@
 import React from "react";
+import { styled } from "@mui/material/styles";
 
+import { Typography } from "@mui/material";
+
+import { useNavigate } from "react-router-dom";
+
+const StyledMainContainer = styled("div")(({ theme }) => ({
+  width: "100%",
+}));
+
+const StyledPageContainer = styled("div")(({ theme }) => ({
+  padding: "0 30vw",
+  margin: "auto",
+  flexWrap: "wrap",
+}));
 
 const LandingPage = () => {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    // handleGithubLogin
+    navigate("/home");
+  };
 
   return (
-    <React.Fragment>
-      LandingPage
-    </React.Fragment>
-  )
-}
+    <StyledMainContainer>
+      <StyledPageContainer>
+        <Typography
+          variant="p"
+          component="h5"
+          sx={{
+            mt: 12,
+            mr: 2,
+            fontWeight: 700,
+          }}
+        >
+          Welcome to the weather forecast web application. Please login with
+          your Github user to use the application and view the weather in your
+          city
+        </Typography>
+        <Typography
+          variant="h6"
+          noWrap
+          component="button"
+          sx={{
+            mr: 2,
+            mt: 6,
+            fontFamily: "Roboto",
+            textDecoration: "none",
+            border: "1px solid rgb(144, 202, 249)",
+            borderRadius: "4px",
+            padding: "5px 15px",
+            cursor: "pointer",
+          }}
+          onClick={handleLoginClick}
+        >
+          Login
+        </Typography>
+      </StyledPageContainer>
+    </StyledMainContainer>
+  );
+};
 
 export default LandingPage;
